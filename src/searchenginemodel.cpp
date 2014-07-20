b9033eefcfab1d4c20be06048d6587ba434725f2
Stuart Howarth <showarth@marxoft.co.uk>
2014-07-20 23:05:02 +0100

    Add SearchEngineModel

---
+++ src/searchenginemodel.cpp
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "searchenginemodel.h"
+#include <QSettings>
+#include <QDir>
+#include <QIcon>
+
+SearchEngineModel::SearchEngineModel(QObject *parent) :
+    QAbstractListModel(parent)
+{
+    QHash<int, QByteArray> roles;
+    roles[NameRole] = "name";
+    roles[IconRole] = "icon";
+    roles[UrlRole] = "url";
+    this->setRoleNames(roles);
+}
+
+SearchEngineModel::~SearchEngineModel() {}
+
+void SearchEngineModel::setFileName(const QString &fileName) {
+    if (fileName != this->fileName()) {
+        m_fileName = fileName;
+        emit fileNameChanged();
+    }
+}
+
+int SearchEngineModel::rowCount(const QModelIndex &parent) const {
+    Q_UNUSED(parent);
+
+    return m_list.size();
+}
+
+QVariant SearchEngineModel::data(const QModelIndex &index, int role) const {
+    if (!index.isValid()) {
+        return QVariant();
+    }
+
+    switch (role) {
+    case Qt::DisplayRole:
+        return m_list.at(index.row()).name;
+    case Qt::DecorationRole:
+        return QIcon(m_list.at(index.row()).icon);
+    case IconRole:
+        return m_list.at(index.row()).icon;
+    case UrlRole:
+        return m_list.at(index.row()).url;
+    default:
+        return QVariant();
+    }
+}
+
+bool SearchEngineModel::setData(const QModelIndex &index, const QVariant &value, int role) {
+    if (!index.isValid()) {
+        return false;
+    }
+
+    switch (role) {
+    case Qt::DisplayRole:
+    case IconRole:
+        m_list[index.row()].icon = value.toString();
+        break;
+    case UrlRole:
+        m_list[index.row()].url = value.toString();
+        break;
+    default:
+        return false;
+    }
+
+    emit dataChanged(index, index);
+
+    QSettings settings(this->fileName(), QSettings::NativeFormat);
+    settings.beginGroup(m_list.at(index.row()).name);
+    settings.setValue("icon", m_list.at(index.row()).icon);
+    settings.setValue("url", m_list.at(index.row()).url);
+    settings.endGroup();
+}
+
+void SearchEngineModel::addSearchEngine(const QString &name, const QString &icon, const QString &url) {
+    QSettings settings(this->fileName(), QSettings::NativeFormat);
+    settings.beginGroup(name);
+    settings.setValue("icon", icon);
+    settings.setValue("url", url);
+    settings.endGroup();
+    this->load();
+}
+
+void SearchEngineModel::removeSearchEngine(const QString &name) {
+    for (int i = 0; i < m_list.size(); i++) {
+        if (m_list.at(i).name == name) {
+            this->removeSearchEngine(i);
+            return;
+        }
+    }
+}
+
+void SearchEngineModel::removeSearchEngine(const QModelIndex &index) {
+    this->removeSearchEngine(index.row());
+}
+
+void SearchEngineModel::removeSearchEngine(int row) {
+    if ((row >= 0) && (row < m_list.size())) {
+        this->beginRemoveRows(QModelIndex(), row, row);
+        QSettings(this->fileName(), QSettings::NativeFormat).remove(m_list.takeAt(row).name);
+        this->endRemoveRows();
+        emit countChanged();
+    }
+}
+
+void SearchEngineModel::load() {
+    QSettings settings(this->fileName(), QSettings::NativeFormat);
+    QDir dir(this->fileName().left(this->fileName().lastIndexOf('/')));
+
+    this->beginResetModel();
+    m_list.clear();
+
+    QStringList groups = settings.childGroups();
+    groups.sort();
+
+    foreach (QString group, groups) {
+        settings.beginGroup(group);
+
+        QString icon = dir.absoluteFilePath(settings.value("icon").toString());
+
+        if (!dir.exists(icon)) {
+            icon = "/usr/share/icons/hicolor/64x64/hildon/general_web.png";
+        }
+
+        SearchEngine se;
+        se.name = group;
+        se.icon = icon;
+        se.url = settings.value("url").toString();
+        m_list.append(se);
+
+        settings.endGroup();
+    }
+
+    this->endResetModel();
+    emit countChanged();
+}
---
+++ src/searchenginemodel.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef SEARCHENGINEMODEL_H
+#define SEARCHENGINEMODEL_H
+
+#include <QAbstractListModel>
+#include <qdeclarative.h>
+
+typedef struct {
+    QString name;
+    QString icon;
+    QString url;
+} SearchEngine;
+
+class SearchEngineModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int count
+               READ rowCount
+               NOTIFY countChanged)
+    Q_PROPERTY(QString fileName
+               READ fileName
+               WRITE setFileName
+               NOTIFY fileNameChanged)
+
+    Q_ENUMS(Roles)
+
+public:
+    enum Roles {
+        NameRole = Qt::DisplayRole,
+        IconRole = Qt::UserRole + 1,
+        UrlRole
+    };
+
+    explicit SearchEngineModel(QObject *parent = 0);
+    ~SearchEngineModel();
+
+    inline QString fileName() const { return m_fileName; }
+    void setFileName(const QString &fileName);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+    Q_INVOKABLE QVariant data(const QModelIndex &index, int role) const;
+    Q_INVOKABLE bool setData(const QModelIndex &index, const QVariant &value, int role);
+
+    Q_INVOKABLE void addSearchEngine(const QString &name, const QString &icon, const QString &url);
+    Q_INVOKABLE void removeSearchEngine(const QString &name);
+    Q_INVOKABLE void removeSearchEngine(const QModelIndex &index);
+    Q_INVOKABLE void removeSearchEngine(int row);
+
+public slots:
+    void load();
+
+signals:
+    void countChanged();
+    void fileNameChanged();
+
+private:
+    QString m_fileName;
+
+    QList<SearchEngine> m_list;
+
+    Q_DISABLE_COPY(SearchEngineModel)
+};
+
+QML_DECLARE_TYPE(SearchEngineModel)
+
+#endif // SEARCHENGINEMODEL_H
